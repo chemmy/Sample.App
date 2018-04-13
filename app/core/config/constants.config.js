461d9efb9ba52a208eb8e906a64c5c3b8f9a5efc
DAV-DT13\chemm <chemmy0430@gmail.com>
2018-04-12 22:39:27 -0700

    Add config file for constants

---
+++ app/core/config/constants.config.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+
+    var app = angular.module('app');
+    
+    app.constant('API', {
+        'END_POINT': 'http://raphaeldevpartners.pythonanywhere.com'
+    });
+
+})();
