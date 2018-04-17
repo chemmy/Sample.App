25fde17ce371b76fc1239594ccde7d587be61fea
DAV-DT13\chemm <chemmy0430@gmail.com>
2018-04-16 18:41:45 -0700

    Add constant configuration for form error messages

--- app/core/config/constants.config.js
+++ app/core/config/constants.config.js
@@ -7,4 +7,9 @@
         'END_POINT': 'http://raphaeldevpartners.pythonanywhere.com'
     });
 
+    app.constant('FORM_CONST', {
+        'API_FETCH_FAILED': 'Unable to load data. Please try again later.',
+        'API_ACTION_FAILED': 'Unable to continue action. Please try again later.'
+    });
+
 })();
